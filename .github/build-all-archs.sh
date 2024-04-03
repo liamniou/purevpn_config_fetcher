1646e27bf6d2d2874a403734bf2ef15c9dc6cfd2
Rikpat <kadlcik.patrik@gmail.com>
2024-04-03 08:14:21 +0200

    feat: add CGO_enabled

--- .github/build-all-archs.sh
+++ .github/build-all-archs.sh
@@ -9,7 +9,7 @@ for platform in $BUILD_ARCHS; do
         output_name+='.exe'
     fi
 
-    env GOOS=$GOOS GOARCH=$GOARCH go build -o $output_name github.com/Rikpat/purevpnwg
+    env GOOS=$GOOS GOARCH=$GOARCH CGO_ENABLED=0 go build -o $output_name github.com/Rikpat/purevpnwg
     if [ $? -ne 0 ]; then
         echo 'An error has occurred! Aborting the script execution...'
         exit 1
